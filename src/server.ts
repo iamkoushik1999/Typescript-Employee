40ddca35bcba5d7b2dba6f3df85d4e02159eafbb
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-06-11 20:59:07 +0530

    add: error middleware created & connected

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from "express";
-import "dotenv/config";
+import express from 'express';
+import 'dotenv/config';
 // DATABASE
-import connectDB from "./config/db"
+import connectDB from './config/db';
+// ERROR MIDDLEWARE
+import errorHandler from './middlewares/errorMiddleware';
 // ENV
 const { PORT } = process.env;
 // App Initialize
@@ -12,7 +14,10 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Error Handler
+app.use(errorHandler);
+
 // SERVER
 app.listen(PORT, () => {
-   console.log(`Server running on: ${PORT}`);
- });
+  console.log(`Server running on: ${PORT}`);
+});
